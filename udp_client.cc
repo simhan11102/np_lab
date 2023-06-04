60501eaeea41e4bd743725da98a7fa28150b3007
simhan11102 <116964711+simhan11102@users.noreply.github.com>
2023-06-04 23:44:30 +0530

    udp_client.cc

--- udp_client.cc
+++ udp_client.cc
@@ -9,7 +9,6 @@ void send_file_data(FILE *fp, int sockfd, struct sockaddr_in addr){
   int n;
   char buffer[SIZE];
 
-  // Sending the data
   while(fgets(buffer, SIZE, fp) != NULL){
     printf("[SENDING] Data: %s", buffer);
 
@@ -22,7 +21,7 @@ void send_file_data(FILE *fp, int sockfd, struct sockaddr_in addr){
 
   }
 
-  // Sending the 'END'
+
   strcpy(buffer, "END");
   sendto(sockfd, buffer, SIZE, 0, (struct sockaddr*)&addr, sizeof(addr));
 
@@ -32,17 +31,16 @@ void send_file_data(FILE *fp, int sockfd, struct sockaddr_in addr){
 
 int main(){
 
-  // Defining the IP and Port
+
   char *ip = "127.0.0.1";
   int port = 8080;
 
-  // Defining variables
+
   int server_sockfd;
   struct sockaddr_in server_addr;
   FILE *fp;
   char *filename = "client.txt";
 
-  // Creating a UDP socket
   server_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
   if (server_sockfd < 0){
     perror("[ERROR] socket error");
@@ -52,14 +50,13 @@ int main(){
   server_addr.sin_port = port;
   server_addr.sin_addr.s_addr = inet_addr(ip);
 
-  // Reading the text file
+
   fp = fopen(filename, "r");
   if (fp == NULL){
     perror("[ERROR] reading the file");
     exit(1);
   }
 
-  // Sending the file data to the server
   send_file_data(fp, server_sockfd, server_addr);
 
   printf("[SUCCESS] Data transfer complete.\n");
